be1cacd54bd1683322f0d0b5cef6349091eb6c20
Guy Eschemann <Guy.Eschemann@plc2.de>
2022-06-02 15:34:18 +0200

    switch to apache license

--- components/pulse_synchronizer.vhd
+++ components/pulse_synchronizer.vhd
@@ -1,5 +1,4 @@
 -------------------------------------------------------------------------------
---
 --  Pulse-synchronizer for clock-domain crossings.
 --
 --  This file is part of the noasic library.
@@ -10,29 +9,20 @@
 --
 --  Author(s):
 --    Guy Eschemann, guy@noasic.com
---
 -------------------------------------------------------------------------------
---
---  Copyright (c) 2014 Guy Eschemann
---
---  This source file may be used and distributed without restriction provided
---  that this copyright statement is not removed from the file and that any
---  derivative work contains the original copyright notice and the associated
---  disclaimer.
---
---  This source file is free software: you can redistribute it and/or modify it
---  under the terms of the GNU Lesser General Public License as published by
---  the Free Software Foundation, either version 3 of the License, or (at your
---  option) any later version.
---
---  This source file is distributed in the hope that it will be useful, but
---  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
---  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
---  for more details.
---
---  You should have received a copy of the GNU Lesser General Public License
---  along with the noasic library.  If not, see http://www.gnu.org/licenses
---
+-- Copyright (c) 2012-2022 Guy Eschemann
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -------------------------------------------------------------------------------
 
 library ieee;
